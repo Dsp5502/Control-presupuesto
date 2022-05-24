ce85c77c82183e231125d7e86d2c35e0b3fc8616
David Puerto Guerrero <dsp5502@gmail.com>
2022-05-24 16:02:13 -0500

    Formatear Números a Dinero

--- src/components/ControlPreuspuesto.jsx
+++ src/components/ControlPreuspuesto.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 
 const ControlPreuspuesto = ({ presupuesto }) => {
+  const formatearCantidad = (cantidad) => {
+    return cantidad.toLocaleString('es-US', {
+      style: 'currency',
+      currency: 'USD',
+    });
+  };
   return (
     <div className='contenedor-presupuesto contenedor sombra dos-columnas'>
       <div>
@@ -8,7 +14,13 @@ const ControlPreuspuesto = ({ presupuesto }) => {
       </div>
       <div className='contenido-presupuesto'>
         <p>
-          <span>Presupuesto: </span> ${presupuesto}
+          <span>Presupuesto: </span> {formatearCantidad(presupuesto)}
+        </p>
+        <p>
+          <span>Disponible: </span> {formatearCantidad(0)}
+        </p>
+        <p>
+          <span>Gastado: </span> {formatearCantidad(0)}
         </p>
       </div>
     </div>
